94970816e92c500462eee3e68613c4845779e9ed
titan080708 <ldg1887@gmail.com>
2025-05-27 18:59:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-import streamlit as at 
+# streamlit_example.py
 
-st.title("나의 첫번째 앱")
+import streamlit as st
+
+# 제목
+st.title("간단한 제곱 계산기")
+
+# 숫자 입력 받기
+number = st.number_input("숫자를 입력하세요", value=0)
+
+# 버튼 누르면 계산
+if st.button("제곱 계산하기"):
+    squared = number ** 2
+    st.success(f"{number}의 제곱은 {squared}입니다.")
